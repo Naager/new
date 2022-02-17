20c84e430063e387459df70fe7ee9c883687db83
Naage <naager@gmail.com>
2022-02-17 16:32:34 +0200

    начальный вариант

--- script.js
+++ script.js
@@ -1,31 +1,92 @@
 "use strict";
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['1', '2', '3', '4', '5', '6', '7', '8', '9',' 0'];
+const action =['-', '+', 'x', '/'];
+
+
+const out = document.querySelector('.calc_result');
 
 
 
-function plus(x,y) {
- return +(x + y);
-}
+document.querySelector('.clearAll').addEventListener('click',  clearAll => {   /*Нажатие кнопки АС котороя очищает поле ввода*/
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+});
 
-function minus(x,y) {
-    return +(x - y);
-}
+document.querySelector('.calc').addEventListener('click', event => { /*Поведение кнопок*/
 
-function multiply(x,y){
-    return +(x * y);
-}
+   if (!event.target.classList.contains("calc_btn")) {                                   /* Если клик не на кнопку то ничего не происходит*/
+       a = '';
+       b = '';
+       sign = '';
+       finish = false;
+       out.textContent = 0;
+   };
+   if (event.target.classList.contains("ac")) return;                                    /* Нажатие кнопки АС не вносит символы в переменную*/
+   out.textContent = '';
+   let key = event.target.textContent;
 
-function divide(x,y) {
-    return +(x / y);
-}
+   if (digit.includes(key)) {                                                            /* Если переменные 'b' и 'sign' пустые то заполняется переменная 'a'*/
+       if (b === '' && sign === '') {
 
-function square(x) {
-    return +(x * x);
-}
+       a += key;
+       console.log(a, b, sign);
+       out.textContent = a;
+       }
+       else if (a !== '' && b !== '' && finish) {                                        /* Если переменные 'a' и 'b' не пустые и нажато '='  найденое значение присваивается 'a' */
+           b = key;
+           finish = false;
+           out.textContent = b;
 
-function elevate(x,y) {
-    let res = x;
-    for (let i = 1; i < y; i++ ){
-         res = res * x;
-    }
-        return res;
-}
+       }
+       else {                                                                            /* В остальных случаях наполняем 'b' */
+           b += key;
+           out.textContent = b;
+       }
+           console.log(a, b, sign);
+           return;
+   }
+
+   if (action.includes(key)){
+       sign = key;
+       console.log(a, b, sign);
+       out.textContent = sign;
+   }
+   if (key === '=') {                                                                   /* Равно и вычисления*/
+       if (b === '') b = a;                                                             /* Если 'b' равно пустой строке то 'b' присвоить значение 'a' */
+       switch (sign) {
+           case "+":
+               a = (+a) + (+b);
+               break;
+           case "-":
+               a = a - b;
+               break;
+           case "*":
+               a = a * b;
+               break;
+           case "/":
+               if (b === '0') {                                                          /* 0 x 0 */
+                   out.textContent = "Мы все умрем";
+                   a = '';
+                   b = '';
+                   sign = '';
+                   return;
+               }
+               a = a / b;
+               break;
+       }
+
+       finish = true;
+       out.textContent = a;
+       console.log(a, b, sign);
+   }
+
+});
+
